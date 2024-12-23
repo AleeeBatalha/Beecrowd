f3a843333705e9f65ec7514abee914cf0b8d61d4
Francisco Batalha <francisco@warrior.br>
2024-12-23 20:58:03 -0300

    third commit

---
+++ Python/Beecrowd 1002.py
@@ -0,0 +1,18 @@
+'''
+import math
+
+X = math.pi
+Circle = float(input())
+
+multi = X * Circle**2
+
+print(f"A = {multi:.4f}")
+
+'''
+X = 3.14159
+
+Circle = float(input())
+
+multi = X * Circle**2
+
+print(f"A = {multi:.4f}")
