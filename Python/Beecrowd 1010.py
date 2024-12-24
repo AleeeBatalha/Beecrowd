ede3bc8b01773342b087aa6cf5eeb78583c82339
Francisco Batalha <you@example.com>
2024-12-24 12:33:43 -0300

    up

--- Python/Beecrowd 1010.py
+++ Python/Beecrowd 1010.py
@@ -1,8 +1,8 @@
-a,b,c=[float(x) for x in input().split()]
-d,e,f=[float(x) for x in input().split()]
+A, B, C=[float(x) for x in input().split()]
+D, E, F=[float(x) for x in input().split()]
 
-calc1 = b * c
-calc2 = e * f
+calc1 = B * C
+calc2 = E * F
 calcFinal = calc1 + calc2
 
 print(f"VALUE TO PAY = R$ {calcFinal:.2f}")
---
+++ Python/Beecrowd 1013.py
@@ -0,0 +1,9 @@
+'''
+
+A, B, C=[int(x) for x in input().split()]
+
+maior = (A + B + abs (A - B)) / 2
+
+print(maior)
+
+'''
