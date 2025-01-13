90ae119f7f8e52542ad48489077d966ea8ec98ad
AleeeBatalha <porygonzcyber@outlook.com>
2025-01-13 15:07:01 -0300

    Create Beecrowd 2936.py

---
+++ Python/Beecrowd 2936.py
@@ -0,0 +1,16 @@
+curupira = int(input())
+boitata = int(input())
+boto = int(input())
+mapinguari = int(input())
+iara = int(input())
+
+gmcu = 300
+gmbo = 1500
+gmto = 600
+gmma = 1000
+gmia = 150
+gmch = 225
+
+soma = (curupira * gmcu) + (boitata * gmbo) + (boto * gmto) + (mapinguari * gmma) + (iara * gmia) + gmch
+
+print(f"{soma}")
